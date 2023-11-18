6c005d9178d9dad026fcfc52c2c9cbf3808b2f36
Sanjib Sonowal <rocery.cto@gmail.com>
2023-11-18 20:11:12 +0530

    Decision Tree and Random Forest algo for calculating credit score

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 BASE_DIR = os.path.dirname(__file__)
@@ -30,16 +32,38 @@ def logistic_regression_predict():
     pd.DataFrame({"actual_value": y_test, "predicted_value": y_predict})
     # Print prediction accuracy score
     score = accuracy_score(y_test, y_predict)
-    print(accuracy_score(y_test, y_predict))
+    print(score)
     return score
 
 
 def decision_tree_predict():
-    return None
+    df = pd.read_csv('model.csv')
+    X = df.drop(columns=["Credit_Score"])
+    y = df["Credit_Score"]
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    dt = DecisionTreeClassifier()
+    dt.fit(x_train, y_train)
+    y_predict = dt.predict(x_test)
+    pd.DataFrame({"actual_value": y_test, "predicted_value": y_predict})
+    # Print prediction accuracy score
+    score = accuracy_score(y_test, y_predict)
+    print(score)
+    return score
 
 
 def random_forest_predict():
-    return None
+    df = pd.read_csv('model.csv')
+    X = df.drop(columns=["Credit_Score"])
+    y = df["Credit_Score"]
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    rf = RandomForestClassifier()
+    rf.fit(x_train, y_train)
+    y_predict = rf.predict(x_test)
+    pd.DataFrame({"actual_value": y_test, "predicted_value": y_predict})
+    # Print prediction accuracy score
+    score = accuracy_score(y_test, y_predict)
+    print(score)
+    return score
 
 
 if __name__ == "__main__":
